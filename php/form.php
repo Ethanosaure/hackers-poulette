8f901d76f1f1e5c40c4a563c10dbe68d65a48d77
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-19 14:00:06 +0200

    adding honeypot

--- php/form.php
+++ php/form.php
@@ -10,47 +10,44 @@ if(isset($_POST['button'])){
     $firstname=$_POST['firstname'];
     $email=$_POST['email'];
     $description=$_POST['description'];
+    $honey = $_POST['honey'];
 
     $sanitized_name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
     $sanitized_firstname = htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8');
     $sanitized_email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $sanitized_description = str_replace(['<', '>', '$', '#' , '[', ']', '}', '{'], '', $description);
 
+    
     $check = true;
-    if ($sanitized_name !== $name ){
-        $check = false;
-        echo 'remove the special characters from your "name" input'."<br>";
-    }
-    if (empty($sanitized_name)){
-        $check = false;
-        echo 'the "name" input should not be empty'."<br>";
-    }
-
-
+    if ($honey) {
+    echo 'bien tenté le spam gros nullos';
+    exit();
+    }else {
      if ($sanitized_firstname !== $firstname ){
         $check = false;
-        echo 'remove the special characters from your "firstname" input'."<br>";
     }
     if (empty($sanitized_firstname)){
         $check = false;
-        echo 'the "firstname" input should not be empty'."<br>";
     }
-
-
-     if ($sanitized_description !== $description ){
+     if (isset($_POST['button'])){
+        if ($sanitized_name !== $name ){
         $check = false;
-        echo 'remove the special characters from your "description" input'."<br>" ;
+    }
+    if (empty($sanitized_name)){
+        $check = false;
+    }
+    if (!filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+        $check = false;
+    }
+    if ($sanitized_description !== $description ){
+        $check = false;
+       
     }
     if (empty($sanitized_description)){
         $check = false;
-        echo 'the "description" input should not be empty'."<br>" ;
+      
     }
-    
-
-    if (!filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
-        $check = false;
-        echo 'email invalid'."<br>";
-    }
+   
 
     if ($check){
         $req = 'INSERT INTO support (name, firstname, email, description) VALUES (?,?,?,?)';
@@ -62,9 +59,12 @@ if(isset($_POST['button'])){
         $query->execute();
         echo 'envoyé avec succes';
         } else {
-            echo 'error';
+            echo 'error: please enter your information correclty';
         }
     }
+}
+
+}
 
 ?>
 
@@ -79,12 +79,57 @@ if(isset($_POST['button'])){
     <form method='POST'>
         <label for='name'>name:</label>
         <input type='text' name='name' value='' placeholder="name"> <br>
+         <?php
+        if (isset($_POST['button'])){
+        if ($sanitized_name !== $name ){
+        $check = false;
+        echo 'remove the special characters from your "firstname" input'."<br>";
+        }
+        if (empty($sanitized_name)){
+        $check = false;
+        echo 'the "name" input should not be empty'."<br>";
+         }
+        }
+        ?>
         <label for='firstname'>firstname:</label>
         <input type='text' name='firstname' value='' placeholder='firstname'><br>
+        <?php
+        if (isset($_POST['button'])){
+          if ($sanitized_firstname !== $firstname ){
+        $check = false;
+        echo 'remove the special characters from your "firstname" input'."<br>";
+        }
+        if (empty($sanitized_firstname)){
+        $check = false;
+        echo 'the "firstname" input should not be empty'."<br>";
+            }
+    }
+            ?>
         <label for='email'>email:</label>
         <input type ='email' name='email' value='' placeholder='email'><br>
+        <?php
+        if (isset($_POST['button'])){
+    if (!filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+        $check = false;
+        echo 'email invalid'."<br>";
+        }
+        }
+    ?>
         <label for='description'>description:</label>
         <input type='text' name='description' value='' placeholder='max 1000 char.'><br>
+        <?php
+        if (isset($_POST['button'])){
+    if ($sanitized_description !== $description ){
+        $check = false;
+        echo 'remove the special characters from your "description" input'."<br>" ;
+    }
+    if (empty($sanitized_description)){
+        $check = false;
+        echo 'the "description" input should not be empty'."<br>" ;
+    }
+    }
+    ?>
+    <input type="text" name='honey' value=''>
         <button type='submit' name='button'>Send</button>
     </form>
     
