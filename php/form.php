f4130ba8879233a3c4f0a851d157ca3ad6a471d1
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-19 14:08:53 +0200

    hidding honeypot

--- php/form.php
+++ php/form.php
@@ -73,6 +73,7 @@ if(isset($_POST['button'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Contact Support</title>
 </head>
 <body>
@@ -129,7 +130,7 @@ if(isset($_POST['button'])){
     }
     }
     ?>
-    <input type="text" name='honey' value=''>
+    <input type="text" name='honey' class="honey" value=''><br>
         <button type='submit' name='button'>Send</button>
     </form>
     
