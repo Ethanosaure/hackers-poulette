2c53457cbe62eaac27c9e55cda5c34655805260c
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-19 09:49:58 +0200

    adding form

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+require 'access.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Support</title>
+</head>
+<body>
+    <form>
+        <input type='text' name='name' value=''>
+        <input type='text' name='firstname' value=''>
+        <input type ='email' name='email' value=''>
+        <input type='text' name='description' value=''>
+
+
+
+
+    </form>
+    
+</body>
+</html>
