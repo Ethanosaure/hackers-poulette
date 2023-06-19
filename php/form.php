5429d148cfd5e92714d9629f71d4b8bdc46af702
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-19 10:52:46 +0200

    creating query

--- php/form.php
+++ php/form.php
@@ -1,5 +1,21 @@
 <?php
 require 'access.php';
+
+
+
+
+
+if(isset($_POST['button'])){
+    $_POST['name']=$name;
+    $_POST['firstname']=$firstname;
+    $_POST['email']=$email;
+    $_POST['description']=$description;
+
+    $req = 'INSERT INTO support (name, firstname, email, description) VALUES (?,?,?,?)';
+$query = $bdd->prepare([$name, $firstname, $email, $desciption]);
+$query->execute();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,19 +26,16 @@ require 'access.php';
     <title>Contact Support</title>
 </head>
 <body>
-    <form>
+    <form method='POST'>
         <label for='name'>name</label>
-        <input type='text' name='name' value='' placeholder="please enter your name"> <br>
+        <input type='text' name='name' value='' placeholder="name"> <br>
         <label for='firstname'>firstname</label>
-        <input type='text' name='firstname' value='' placeholder='please enter your firstname'><br>
+        <input type='text' name='firstname' value='' placeholder='firstname'><br>
         <label for='email'>email</label>
-        <input type ='email' name='email' value='' placeholder='please enter your email'><br>
+        <input type ='email' name='email' value='' placeholder='email'><br>
         <label for='description'>description</label>
-        <input type='text' name='description' value='' placeholder='please explain your problem'><br>
-
-
-
-
+        <input type='text' name='description' value='' placeholder='explain your problem'><br>
+        <button type='submit' name='button'>Send</button>
     </form>
     
 </body>
