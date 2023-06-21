3b18719f59deeea41347e22cfa8609231987a9cb
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-21 16:08:09 +0200

    adding PHP mailer

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require 'access.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 
 if (isset($_POST['button'])) {
     $name = $_POST['name'];
