36166af5e7a1cd31f89ed3bc4825f9086da30e3f
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-21 16:38:25 +0200

    resolving some bugs

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (isset($_POST['button'])) {
                 'message' => 'Image dimensions should be within 300x200.'
             );
         } 
-        move_uploaded_file($_FILES['file']['tmp_name'], $target_path)
+        move_uploaded_file($_FILES['file']['tmp_name'], $target_path);
         }
     }
 
