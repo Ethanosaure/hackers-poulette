f115e5076968dec8b88d3b62916e73762809fd5d
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-21 11:54:35 +0200

    creating webhost

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'access.php';
+require 'api/src/php/access.php';
 
 if (isset($_POST['button'])) {
     $name = $_POST['name'];
@@ -127,7 +127,7 @@ if (isset($_POST['button'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\css\style.css">
+    <link rel="stylesheet" href="api/src/css\style.css">
     <title>Contact Support</title>
 </head>
 <body>
